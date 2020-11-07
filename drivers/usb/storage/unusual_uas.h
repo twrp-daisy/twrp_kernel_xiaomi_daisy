7f36c55963bacf633274c2ee103b20465a8d7972
Albert I <kras@raphielgang.org>
2020-11-07 22:51:01 +0800

    Merge branch 'android-4.9.235-q-merge' into a11
    * android-4.9.235-q-merge: (80 commits)
      ANDROID: overflow.h: fix merge issue with 4.9.235
      Linux 4.9.235
      ALSA: usb-audio: Update documentation comment for MS2109 quirk
      HID: hiddev: Fix slab-out-of-bounds write in hiddev_ioctl_usage()
      btrfs: check the right error variable in btrfs_del_dir_entries_in_log
      usb: storage: Add unusual_uas entry for Sony PSZ drives
      USB: gadget: u_f: Unbreak offset calculation in VLAs
      USB: gadget: f_ncm: add bounds checks to ncm_unwrap_ntb()
      USB: gadget: u_f: add overflow checks to VLA macros
      overflow.h: Add allocation size calculation helpers
      usb: host: ohci-exynos: Fix error handling in exynos_ohci_probe()
      USB: Ignore UAS for JMicron JMS567 ATA/ATAPI Bridge
      USB: quirks: Add no-lpm quirk for another Raydium touchscreen
      usb: uas: Add quirk for PNY Pro Elite
      USB: yurex: Fix bad gfp argument
      device property: Fix the secondary firmware node handling in set_primary_fwnode()
      PM: sleep: core: Fix the handling of pending runtime resume requests
      xhci: Do warm-reset when both CAS and XDEV_RESUME are set
      XEN uses irqdesc::irq_data_common::handler_data to store a per interrupt XEN data pointer which contains XEN specific information.
      writeback: Fix sync livelock due to b_dirty_time processing
      ...
    
     Conflicts:
    	drivers/scsi/ufs/ufshcd.c
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- drivers/usb/storage/unusual_uas.h
+++ drivers/usb/storage/unusual_uas.h
@@ -48,6 +48,13 @@ UNUSUAL_DEV(0x054c, 0x087d, 0x0000, 0x9999,
 		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
 		US_FL_NO_REPORT_OPCODES),
 
+/* Reported-by: Julian Groß <julian.g@posteo.de> */
+UNUSUAL_DEV(0x059f, 0x105f, 0x0000, 0x9999,
+		"LaCie",
+		"2Big Quadra USB3",
+		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
+		US_FL_NO_REPORT_OPCODES),
+
 /*
  * Apricorn USB3 dongle sometimes returns "USBSUSBSUSBS" in response to SCSI
  * commands in UAS mode.  Observed with the 1.28 firmware; are there others?
@@ -155,6 +162,13 @@ UNUSUAL_DEV(0x152d, 0x0578, 0x0000, 0x9999,
 		"JMS567",
 		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
 		US_FL_BROKEN_FUA),
+
+/* Reported-by: Thinh Nguyen <thinhn@synopsys.com> */
+UNUSUAL_DEV(0x154b, 0xf00d, 0x0000, 0x9999,
+		"PNY",
+		"Pro Elite SSD",
+		USB_SC_DEVICE, USB_PR_DEVICE, NULL,
+		US_FL_NO_ATA_1X),
 
 /* Reported-by: Hans de Goede <hdegoede@redhat.com> */
 UNUSUAL_DEV(0x2109, 0x0711, 0x0000, 0x9999,
